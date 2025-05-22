fd696435f48755a3f78a3e72226e418ced7feab7
anton <anton.zolochevskiy@gmail.com>
2025-05-22 14:30:04 +0100

    Add working map with points

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,13 +1,36 @@
 import { PermissionsAndroid, Platform, StyleSheet, View } from "react-native";
 
 import Mapbox from "@rnmapbox/maps";
+import type { FeatureCollection, Point } from "geojson";
 import { useEffect, useRef, useState } from "react";
+
+interface PointProperties {
+  title: string;
+}
 
 Mapbox.setAccessToken(
   "pk.eyJ1IjoiaGlrZXdpc2UiLCJhIjoiY21hcGZ2MnFrMGVxdjJscjA3d3M2ejg2biJ9.BnOHZXq733sdJwvu1K_2-Q"
 );
+Mapbox.Logger.setLogLevel("verbose");
+
+const points = [
+  { id: "1", coordinates: [0.091755, 52.210914], title: "WGB" },
+  { id: "2", coordinates: [0.124573, 52.20117], title: "Downing College" },
+  { id: "3", coordinates: [0.122388, 52.211565], title: "Jesus Green" },
+];
+
+const geojson: FeatureCollection<Point, PointProperties> = {
+  type: "FeatureCollection",
+  features: points.map((p) => ({
+    type: "Feature",
+    id: p.id,
+    properties: { title: p.title },
+    geometry: { type: "Point", coordinates: p.coordinates },
+  })),
+};
 
 export default function TabTwoScreen() {
+  const [mapReady, setMapReady] = useState(false);
   const [currentLocation, setCurrentLocation] =
     useState<Mapbox.Location | null>(null);
   const cameraRef = useRef<Mapbox.Camera>(null);
@@ -20,10 +43,13 @@ export default function TabTwoScreen() {
     }
   }, []);
 
+  useEffect(() => {});
+
   function onUpdateLocation(location: Mapbox.Location) {
     if (!currentLocation) {
       cameraRef.current?.setCamera({
         centerCoordinate: [location.coords.longitude, location.coords.latitude],
+        zoomLevel: 10,
       });
     }
     setCurrentLocation(location);
@@ -32,13 +58,45 @@ export default function TabTwoScreen() {
   return (
     <View style={styles.page}>
       <View style={styles.container}>
-        <Mapbox.MapView style={styles.map} styleURL={Mapbox.StyleURL.Light}>
-          <Mapbox.Camera ref={cameraRef} zoomLevel={14} />
+        <Mapbox.MapView
+          style={styles.map}
+          styleURL={Mapbox.StyleURL.Outdoors}
+          onDidFinishRenderingMapFully={() => setMapReady(true)}
+        >
+          <Mapbox.Camera ref={cameraRef} zoomLevel={10} />
           <Mapbox.UserLocation
             visible
             onUpdate={onUpdateLocation}
             showsUserHeadingIndicator
           />
+          <Mapbox.ShapeSource
+            id="pointsSource"
+            shape={geojson}
+            onPress={(e) => {
+              const { title } = e.features[0].properties as PointProperties;
+              console.log(`Pressed: ${title}`);
+              cameraRef.current?.setCamera({
+                centerCoordinate: [
+                  e.coordinates.longitude,
+                  e.coordinates.latitude,
+                ],
+                zoomLevel: 15,
+              });
+            }}
+          >
+            <Mapbox.SymbolLayer
+              id="pointsSymbols"
+              sourceLayerID="pointsSource"
+              style={{
+                iconImage: "marker-15", // built-in Mapbox icon
+                iconAllowOverlap: true, // don’t hide overlapping points
+                iconSize: 2.5, // make it a bit bigger
+                textField: ["get", "title"], // optional: show text labels
+                textOffset: [0, 1.2], // push text above the icon
+                textAllowOverlap: false,
+              }}
+            />
+          </Mapbox.ShapeSource>
         </Mapbox.MapView>
       </View>
     </View>
@@ -52,8 +110,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   container: {
-    height: 300,
-    width: 300,
+    width: 400,
+    height: 800,
+    // flexGrow: 1,
   },
   map: {
     flex: 1,
