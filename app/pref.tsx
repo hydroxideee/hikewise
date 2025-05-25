fda40f65f88532c0c1b2c75868b6f2d2d1920be7
Voltaria <edwardaread@gmail.com>
2025-05-25 11:39:14 +0100

    missed a few touches - added comments and remove favourites

--- app/pref.tsx
+++ app/pref.tsx
@@ -126,7 +126,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.1,
     shadowOffset: { width: 0, height: 4 },
     shadowRadius: 10,
-    elevation: 3, // Android shadow
+    elevation: 3,
   },
   pageTitle: {
     fontSize: 22,
